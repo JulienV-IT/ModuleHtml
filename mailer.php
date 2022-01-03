e202d8576d7b0ac43bdaf4faeb10ad9165d4c5d3
zegzer <zegzer@debian-BULLSEYE-live-builder-AMD64>
2022-01-03 11:52:55 +0400

    [ADD] flex & form + phpmail

---
+++ mailer.php
@@ -0,0 +1,27 @@
+<?php 
+
+	/*$post = $_POST;
+
+	var_dump($post);*/
+	
+
+
+	/*echo $_POST;*/
+
+
+	$get = $_GET;
+	/*var_dump($get);	*/
+	echo $get["fname"];
+	echo("<br>");
+	echo $get["lname"];
+	echo("<br>");
+	echo $get["mail"];
+	echo("<br>");
+
+
+
+
+
+
+
+?>
